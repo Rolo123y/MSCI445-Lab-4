a17a8149ef9a84baba435dd80d09b287b0b8c477
Rolo123y <52054023+Rolo123y@users.noreply.github.com>
2021-07-05 11:16:23 -0400

    initial commit

---
+++ Ping_Client.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Ping_Client
+{
+    class Ping_Client
+    {
+        static void Main(string[] args)
+        {
+            string val;
+            Console.Write("Enter your port number: ");
+            val = Console.ReadLine();
+
+            if (val.Length < 4 || val.Length > 5)
+            {
+                Console.WriteLine("Required arguments: the length of the port number should be between 4 and 5!");
+                return;
+            }
+            if (val.All(char.IsDigit) == false)
+            {
+                Console.WriteLine("Invalid arguments: port number should only contain positive integer digits!");
+                return;
+            }
+            if (Convert.ToInt32(val) <= 1024)
+            {
+                Console.WriteLine("Invalid arguments: port number should be greater than 1024!");
+                return;
+            }
+            int port = Convert.ToInt32(val);
+            string host = "127.0.0.1";
+            create_Client(host, port);
+        }
+
+        public static void create_Client(string host, int port)
+        {
+            UdpClient udpClient = new UdpClient();
+            udpClient.Connect(host, port);
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, port);
+
+            send_N_Pings(10, RemoteIpEndPoint, udpClient);
+        }
+
+        public static void send_N_Pings(int N, IPEndPoint RemoteIpEndPoint, UdpClient udpClient)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("CLIENT SENT PING: " + i + " TIME: " + System.DateTime.Now.TimeOfDay + "\r\n");
+                udpClient.Send(Array.Empty<byte>(), 0);
+
+                var task = Task.Run(() => udpClient.Receive(ref RemoteIpEndPoint));
+                if (task.Wait(TimeSpan.FromSeconds(1)))
+                {
+                    Console.WriteLine("CLIENT RECIEVED PING: " + i + " TIME: " + System.DateTime.Now.TimeOfDay + "\r\n");
+                }
+                else { 
+                Console.WriteLine("SERVER LOST PACKET PING: " + i + " TIME: " + System.DateTime.Now.TimeOfDay + "\r\n");
+                    continue;
+                }
+            }
+        }
+    }
+}
---
+++ Ping_Server.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+
+namespace Ping_Server
+{
+    class Ping_Server
+    {
+        private const double LOSS_RATE = 0.3;
+        private const double AVG_DELAY = 100; //milliseconds
+        static void Main(string[] args)
+        {
+            string val;
+            Console.Write("Enter your port number: ");
+            val = Console.ReadLine();
+
+            if (val.Length < 4 || val.Length > 5)
+            {
+                Console.WriteLine("Required arguments: the length of the port number should be between 4 and 5!");
+                return;
+            }
+            if (val.All(char.IsDigit) == false)
+            {
+                Console.WriteLine("Invalid arguments: port number should only contain positive integer digits!");
+                return;
+            }
+            if (Convert.ToInt32(val) <= 1024)
+            {
+                Console.WriteLine("Invalid arguments: port number should be greater than 1024!");
+                return;
+            }
+            int port = Convert.ToInt32(val);
+            string host = "127.0.0.1";
+
+            IPEndPoint localpt = new IPEndPoint(IPAddress.Any, port);
+            // Create random number generator for use in simulating
+            // packet loss and network delay.
+            Random random = new Random();
+            // Create a datagram socket for receiving and sending UDP packets
+            // through the port specified on the command line.
+            UdpClient socket = new UdpClient();
+            socket.Client.SetSocketOption(SocketOptionLevel.Socket,
+           SocketOptionName.ReuseAddress, true);
+            socket.Client.Bind(localpt);
+            System.Net.IPEndPoint ep = null;
+
+            // Processing loop.
+            while (true)
+            {
+                // Block until the host receives a UDP packet.
+                byte[] rdata = socket.Receive(ref ep);
+                Console.WriteLine("SERVER: Received UDP data" + " TIME: " + System.DateTime.Now.TimeOfDay + "\n");
+                // Print the recieved data.
+                string name_string = Encoding.ASCII.GetString(rdata);
+                Console.WriteLine("SERVER: Just received: " + name_string + " TIME: " + System.DateTime.Now.TimeOfDay + "\n");
+
+                // Decide whether to reply, or simulate packet loss.
+                if (random.NextDouble() < LOSS_RATE)
+                {
+                    Console.WriteLine("SERVER: Reply not sent." + " TIME: " + System.DateTime.Now.TimeOfDay + "\n");
+                    continue;
+                }
+                // Simulate network delay.
+                Thread.Sleep((int)(random.NextDouble() * 2 * AVG_DELAY));
+                // Send reply.
+                socket.Send(rdata, rdata.Length, ep);
+                Console.WriteLine("SERVER: Reply sent." + " TIME: " + System.DateTime.Now.TimeOfDay + "\n");
+            }
+        }
+    }
+}
