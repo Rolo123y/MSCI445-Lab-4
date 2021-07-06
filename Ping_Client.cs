5d44c5a3a18853d6b2537ae0eebea3f86a4ed74b
gaurav613 <gigs612@gmail.com>
2021-07-06 06:32:14 -0400

    change timeout implementation, clean up output, add documentatiion

--- Ping_Client.cs
+++ Ping_Client.cs
@@ -14,6 +14,7 @@ namespace Ping_Client
         static void Main(string[] args)
         {
             string val;
+            // get port number from cli input
             Console.Write("Enter your port number: ");
             val = Console.ReadLine();
 
@@ -33,41 +34,51 @@ namespace Ping_Client
                 return;
             }
             int port = Convert.ToInt32(val);
-            string host = "127.0.0.1";
+            string host = "127.0.0.1"; // change to other IP address when capturing packets on wireshark (10.40.111.200)
             create_Client(host, port);
         }
 
+        // method to create the UDP client
         public static void create_Client(string host, int port)
         {
             UdpClient udpClient = new UdpClient();
             udpClient.Connect(host, port);
-            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, port);
+            IPAddress remoteIP = IPAddress.Parse(host);
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(remoteIP, port);
 
+            // send 10 pings from client to server
             send_N_Pings(10, RemoteIpEndPoint, udpClient);
         }
 
+        // method to send N pings from client to server, and display RTT when reply is received
         public static void send_N_Pings(int N, IPEndPoint RemoteIpEndPoint, UdpClient udpClient)
         {
             for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine("CLIENT SENT PING: " + i + " TIME: " + System.DateTime.Now.TimeOfDay + " \r\n");
+                Console.WriteLine("SENT PING: " + i + " AT TIME: " + System.DateTime.Now.TimeOfDay + " \r\n");
+                // Create packet of data
                 byte[] rdata = Encoding.ASCII.GetBytes("PING " + i + " " + System.DateTime.Now.TimeOfDay + " \r\n");
-                Stopwatch stopWatch = new Stopwatch(); 
-                stopWatch.Start(); 
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
+                // Send data
                 udpClient.Send(rdata, rdata.Length);
 
+                // set timeout to receive reply
+                udpClient.Client.ReceiveTimeout = 1000;
 
-                var task = Task.Run(() => udpClient.Receive(ref RemoteIpEndPoint));
-                if (task.Wait(TimeSpan.FromSeconds(1)))
+                try
                 {
+                    udpClient.Receive(ref RemoteIpEndPoint);
                     stopWatch.Stop();
                     TimeSpan RTT = stopWatch.Elapsed;
-                    Console.WriteLine("CLIENT RECIEVED PING: " + i + " RTT: " + RTT.Seconds+"."+RTT.Milliseconds + "seconds \r\n");
+                    Console.WriteLine("Ping Answered RTT= " + RTT.Seconds + "." + RTT.Milliseconds + "seconds \r\n");
                 }
-                else { 
-                Console.WriteLine("PACKET LOST DURING TRANSMISSION PING: " + i + " TIME: " + System.DateTime.Now.TimeOfDay + " \r\n");
-                    continue;
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Packet assumed to be dropped - took more than 1 second for reply.\n");
                 }
+
             }
         }
     }
